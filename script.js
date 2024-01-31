b7a03b3909c914477af2b8f8512c948c30d8e011
Jonathas dos Anjos <127884309+jonathascp@users.noreply.github.com>
2024-01-31 00:49:35 -0300

    Botão de Copiar o texto

--- script.js
+++ script.js
@@ -2,29 +2,35 @@ let areaTexto = document.querySelector(".area_texto");
 const btnCriptar = document.querySelector(".btn_criptografar");
 const btnDescriptar = document.querySelector(".btn_descriptografar");
 const mensagem = document.querySelector('.resultado_mensagem');
+const btnCopy = document.querySelector(".copy");
 const img = document.querySelector('.imagem');
+let novoTexto = '';
 
 const resultado = document.querySelector('.resultado_texto');
 btnCriptar.addEventListener("click", () => {
-    let novoTexto = '';
     let texto = areaTexto.value;
     novoTexto = texto.replaceAll('e', 'enter').replaceAll('i', 'imes').replaceAll('a', 'ai').replaceAll('o', 'ober').replaceAll('u', 'ufat');
     mensagem.innerHTML = '';
     img.style.display = 'none';
     resultado.setAttribute('class', 'resultado_criptografado');
+    btnCopy.style.display = "block";
     resultado.innerHTML = novoTexto;
 
 });
 
 btnDescriptar.addEventListener("click", () => {
-    let novoTexto = '';
     let texto = areaTexto.value;
     novoTexto = texto.replaceAll('enter', 'e').replaceAll('imes', 'i').replaceAll('ai', 'a').replaceAll('ober', 'o').replaceAll('ufat', 'u');
     mensagem.innerHTML = '';
     img.style.display = 'none';
     resultado.setAttribute('class', 'resultado_criptografado');
+    btnCopy.style.display = "block";
     resultado.innerHTML = novoTexto;
 
 });
+
+btnCopy.addEventListener('click', () => {
+    navigator.clipboard.writeText(novoTexto);
+})
 
 
